12f054c1f51257fcca5e255c62fa60dca1018d97
ayoubwe <115227599+ayoubwe@users.noreply.github.com>
2024-10-23 06:21:34 +0100

    Update

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TaskManager', { 
+mongoose.connect('mongodb://localhost:27017/TaskMaster', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }).then(() => {
